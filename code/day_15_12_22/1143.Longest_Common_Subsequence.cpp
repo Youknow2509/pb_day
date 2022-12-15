edaecfd4a1949a0ab32ec9622bc3e2f9f676c3ba
V-Mac <lytranvinh.2509@gmail.com>
2022-12-15 10:03:35 +0700

    10:03 15/12/22 push v_mac

---
+++ code/day_15_12_22/1143.Longest_Common_Subsequence.cpp
@@ -0,0 +1,41 @@
+//https://leetcode.com/problems/longest-common-subsequence/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+    }
+};
+
+int main()
+{
+
+    int n, i = 1, size_nums, x;
+    Solution st;
+
+    cout << "Tese cace: ";
+    cin >> n;
+    cout << endl;
+
+    while (n--)
+    {
+        string text1, text2;
+        cout << "# Case " << i << endl; i++;
+        cout << "Input text 1: "; cin >> text1;
+        cout << endl;
+        cout << "Input text 2: "; cin >> text2;
+        cout << endl;
+        
+        cout << "Output: " << st.longestCommonSubsequence(text1, text2);
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
+
---
+++ code/day_15_12_22/Balanced_string.cpp
@@ -0,0 +1,31 @@
+// https://practice.geeksforgeeks.org/problems/balanced-string1627/1
+//{ Driver Code Starts
+// Initial template for c++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for c++
+
+class Solution {
+  public:
+    string BalancedString(int N) {
+        // code here
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.BalancedString(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
