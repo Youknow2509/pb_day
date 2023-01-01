014a972c779d9c7ed7a35177694ad149287b55dc
V <lytranvinh.2590@gmail.com>
2023-01-01 23:58:54 +0700

    ...

--- code/test.cpp
+++ code/test.cpp
@@ -3,15 +3,37 @@
 
 using namespace std;
 
+struct node{
+    int data;
+    node* next;
+};
+
+node* newnode(int x){
+    node* temp = new node;
+    temp->next = NULL;
+    temp->data = x;
+    return temp;
+}
+
+node* insert (node *node, int data){
+    node->next = newnode(data);
+    return node;
+}
+
+void printll(node *n){
+    node *temp = n;
+    while( n != NULL){
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 int main(){
 
-    string str;
-    int nb = 1;
-    cin >> str;
-    char ch = str[0];
-    for (int i = 1; i < str.length(); i++){
+    int n, k , x;
+    node ll;
 
-    }
+    cin >> n;
+    
 
     return 0;
 }
