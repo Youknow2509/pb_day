9f7ab064aea91a547869644b282de45268b107c2
V-Mac <lytranvinh.2509@gmail.com>
2023-01-02 10:28:41 +0700

    02/01/23 push v_mac

--- code/test.cpp
+++ code/test.cpp
@@ -16,24 +16,54 @@ node* newnode(int x){
 }
 
 node* insert (node *node, int data){
+    if (node == NULL) newnode(data);
     node->next = newnode(data);
     return node;
 }
 
 void printll(node *n){
     node *temp = n;
-    while( n != NULL){
+    while( temp != NULL){
         cout << temp->data << " ";
         temp = temp->next;
     }
 }
+
+node *addAt(node *l, int k, int x){
+	node *p = l;
+	for (int i = 0; i < k-1; i++){
+		p = p->next;
+	}
+	//
+	node *temp = new node;
+	temp->data = x;
+	temp->next = p->next;
+	p->next = temp;
+	
+	return l;
+}
 int main(){
 
     int n, k , x;
-    node ll;
+    node *ll;
 
     cin >> n;
-    
+    cin >> x;
+    ll = insert(ll, x);
+
+    for (int i = 1; i < n; i++){
+        cin >> x;
+        ll = insert(ll, x);
+    }
+    cin >> k;
+
+    ll = addAt(ll, k, x);
+
+
+    printll(ll);
 
     return 0;
 }
+// Input: 4            Output: 1 10 2 3 4
+//        1 2 3 4
+//        1 10
