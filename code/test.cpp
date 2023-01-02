83fc5eaeea344e45e8cf8e47043ea8450e543dfd
V <lytranvinh.2590@gmail.com>
2023-01-02 11:17:50 +0700

    11:17 02/01/23

--- code/test.cpp
+++ code/test.cpp
@@ -1,69 +1,53 @@
 
-#include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-struct node{
+struct Node{
     int data;
-    node* next;
+    Node* next;
 };
 
-node* newnode(int x){
-    node* temp = new node;
+// Tao mot Node 
+Node* newNode(int data){
+    Node* temp = new Node;
     temp->next = NULL;
-    temp->data = x;
+    temp->data = data;
     return temp;
 }
 
-node* insert (node *node, int data){
-    if (node == NULL) newnode(data);
-    node->next = newnode(data);
+// them gia tri vao vi tri cuoi cung cua Node
+Node* insertNode(Node* node, int data){
+    if (node == NULL) node = newNode(data);
+    node->next = newNode(data);
     return node;
 }
 
-void printll(node *n){
-    node *temp = n;
-    while( temp != NULL){
+// in ra man hinh gia tri cac node lien tiep 
+void printNode(Node* node){
+    Node* temp = node; // sao chep Node hien tai ra mot bien moi de thuc hien code du lieu khong bi thay doi
+    while ( temp != NULL){
         cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
-node *addAt(node *l, int k, int x){
-	node *p = l;
-	for (int i = 0; i < k-1; i++){
-		p = p->next;
-	}
-	//
-	node *temp = new node;
-	temp->data = x;
-	temp->next = p->next;
-	p->next = temp;
-	
-	return l;
-}
 int main(){
 
-    int n, k , x;
-    node *ll;
-
+    Node* node = NULL;
+    int n, x , k;
     cin >> n;
     cin >> x;
-    ll = insert(ll, x);
-
+    node = insertNode(node, x);
     for (int i = 1; i < n; i++){
         cin >> x;
-        ll = insert(ll, x);
+        node = insertNode(node, x);
     }
-    cin >> k;
-
-    ll = addAt(ll, k, x);
-
-
-    printll(ll);
+    printNode(node);
 
     return 0;
 }
-// Input: 4            Output: 1 10 2 3 4
-//        1 2 3 4
-//        1 10
+// Input:   	4
+//              1 2 3 4    -----> Output:   1 10 2 3 4
+//              1 10
+
